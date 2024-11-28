c0af4d2e503e1027fbeef190e4a9e3037d9b41d3
rayie-1010 <2ws2ed2rf@gmail.com>
2024-11-28 12:01:13 +0800

    第二區域

---
+++ second.area/data.js
@@ -0,0 +1,341 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "樂高與模型展示架區",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": -0.035618964326406655,
+        "pitch": 0.3740063681548058,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3445683337049505,
+          "pitch": 0.5351200957973905,
+          "rotation": 10.995574287564278,
+          "target": "1-"
+        },
+        {
+          "yaw": 3.0032924090048896,
+          "pitch": 0.33914560799534677,
+          "rotation": 0,
+          "target": "3-2"
+        },
+        {
+          "yaw": 2.543432034579591,
+          "pitch": 0.24084156332231288,
+          "rotation": 5.497787143782138,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.223966462949317,
+          "pitch": 0.7769868880452204,
+          "title": "工作人員",
+          "text": "工作人員身著寶可夢系列遊戲朱內學院制服，就像遊戲內訓練家遇到學長姊般有親切感。"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "歷代遊戲展示區",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 0.07026650235302156,
+        "pitch": 0.5877242928146664,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.519897847193075,
+          "pitch": 0.4274003130602182,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 2.049170619096797,
+          "pitch": 0.31060494552765405,
+          "rotation": 4.71238898038469,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.9020058531186237,
+          "pitch": 0.0875364632856801,
+          "title": "遊戲畫面",
+          "text": "似乎是寶可夢系列遊戲朱/紫擴充內容–零之秘寶內的遊戲畫面展示"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "遊戲卡牌區",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 1.4150949937346482,
+        "pitch": 0.24404854954843458,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.712610236020172,
+          "pitch": 0.3089253447521916,
+          "rotation": 0.7853981633974483,
+          "target": "1-"
+        },
+        {
+          "yaw": -0.4786747076800104,
+          "pitch": 0.34793478893818097,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": 0.08249247141807636,
+          "pitch": 0.33374388271495903,
+          "rotation": 6.283185307179586,
+          "target": "3-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.919090372132609,
+          "pitch": 0.5963723694374998,
+          "title": "對戰區",
+          "text": "訓練家似乎能在這邊進行寶可夢對戰。"
+        }
+      ]
+    },
+    {
+      "id": "3-2",
+      "name": "圓柱展示架2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.87707703313393,
+          "pitch": 0.7930005591708102,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": -2.1913247925736368,
+          "pitch": 0.27483035998858973,
+          "rotation": 7.0685834705770345,
+          "target": "5-"
+        },
+        {
+          "yaw": 1.5141092628032737,
+          "pitch": 0.46796886123317627,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 0.5961405546051282,
+          "pitch": 0.3762673846393749,
+          "rotation": 5.497787143782138,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.029732676954136394,
+          "pitch": 0.23649588253074327,
+          "title": "展示架",
+          "text": "各式種類的寶可夢聚集在此處，如果是財力雄厚的訓練家或許能在此處獲得滿足也說不定。"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "結帳排隊區",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": 0.05090691842839057,
+        "pitch": 0.3071542818571871,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6326073265247256,
+          "pitch": 0.31580949535390523,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 2.6454325862121415,
+          "pitch": 0.2797624952458815,
+          "rotation": 10.995574287564278,
+          "target": "5-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.1075181080442622,
+          "pitch": 0.2815657638858635,
+          "title": "工作人員",
+          "text": "結帳處的工作人員身著寶可夢系列遊戲紫中葡萄學院制服，看上去很穩重的樣子，真令人安心。<div>似乎還有寶可夢購物袋可以做加購喔。</div>"
+        }
+      ]
+    },
+    {
+      "id": "5-",
+      "name": "出口及扭蛋機",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1442863326712427,
+          "pitch": 0.439351212326077,
+          "rotation": 0.7853981633974483,
+          "target": "4-"
+        },
+        {
+          "yaw": -2.583111079674092,
+          "pitch": 0.35238135659245273,
+          "rotation": 0,
+          "target": "3-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.052507085962192335,
+          "pitch": 0.5812950504205858,
+          "title": "扭蛋機",
+          "text": "似乎會定期更換內容，相信自己運氣的訓練家不妨試一試吧。"
+        }
+      ]
+    }
+  ],
+  "name": "寶可夢中心台北環景",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
